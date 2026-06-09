720d9d61b6f81c203762cf0818146d5bf7c8e282
mortiff75 <mortezazare2618@gmail.com>
2026-06-09 18:37:39 +0330

    delete package json

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+
   images: {
     remotePatterns: [
       { hostname: "images-na.ssl-images-amazon.com", protocol: "https" },
